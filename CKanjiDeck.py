70858027b5d3181b373507ea50aa86929d89b5b6
Fred <test@gitmail.com>
2018-02-19 19:53:59 +0900

    Initial

---
+++ CKanjiDeck.py
@@ -0,0 +1,67 @@
+import csv
+from Kanji import CKanji
+
+
+class CKanjiDeck:
+    def DiffDeck(deck1, deck2):
+        diffNb = 0
+        for k, v in deck1._kanjiMap.items():
+            if not v == deck2._kanjiMap[k]:
+                diffNb += 1
+                print("Diff on key: " + k)
+                for idx in range(len(CKanji.listFields)):
+                    field = CKanji.listFields[idx]
+                    if v[field] != deck2[k][field]:
+                        print("  Diff on field: " + field)
+                        print("  1. " + v[field])
+                        print("  2. " + deck2[k][field])
+        if diffNb == 0:
+            print("No difference found")
+        else:
+            print(str(diffNb) + " difference(s) found")
+
+    def __init__(self):
+        self._kanjiMap = {}
+
+    def __iter__(self):
+        return iter(self._kanjiMap.values())
+
+    def __getitem__(self, key):
+        return self._kanjiMap[key]
+
+    def __contains__(self, value):
+        return value in self._kanjiMap
+
+    def CreateDeckFromAnkiFile(self, fileName):
+        with open(fileName, 'r', encoding='utf-8') as fDeck:
+            csvIn = csv.reader(fDeck, delimiter='\t', quotechar='"')
+            for row in csvIn:
+                self._kanjiMap[row[0]] = CKanji(row[0])
+                self._kanjiMap[row[0]].SetFromRow(row)
+
+    def OutputToCsv(self, fileName):
+        with open(fileName, 'w', encoding='utf-8', newline='') as fDeck:
+            csvOut = csv.writer(fDeck, delimiter='\t', quotechar='"')
+            for k, v in self._kanjiMap.items():
+                csvOut.writerow(v)
+
+    def ProcessJitenon(self):
+        for kj in iter(self._kanjiMap.values()):
+            kj.ProcessJitenon()
+
+    def GetNbOfReading(self):
+        nbOfReading = [0, 0, 0, 0]
+        for kj in iter(self._kanjiMap.values()):
+            nbOfReading = [sum(x) for x in zip(nbOfReading, kj.GetNbOfReading())]
+        return nbOfReading
+
+    # Return list: Kanji with OnYomi Gai, KunYomi Gai, eith On or Kun Gai
+    def GetNbOfKanjiWithGaiYomi(self):
+        nb_kanji_with_gai_yomi = [0, 0]
+        nb_with_gai_on_or_kun = 0
+        for kj in iter(self._kanjiMap.values()):
+            has_gai = kj.hasGaiYomi()
+            nb_kanji_with_gai_yomi = [sum(x) for x in zip(nb_kanji_with_gai_yomi, has_gai)]
+            nb_with_gai_on_or_kun += has_gai[0] or has_gai[1]
+        nb_kanji_with_gai_yomi.append(nb_with_gai_on_or_kun)
+        return nb_kanji_with_gai_yomi
---
+++ Kanji.py
@@ -0,0 +1,134 @@
+import html
+import lxml
+from lxml import html
+import re
+import pickle
+import jsonpickle
+
+
+katakana_chart = ("ァアィイゥウェエォオカガキギクグケゲコゴサザシジスズセゼソゾタダチヂッツヅテデトドナニヌネノ"
+                  "ハバパヒビピフブプヘベペホボポマミムメモャヤュユョヨラリルレロヮワヰヱヲンヴヵヶヽヾ")
+hiragana_chart = ("ぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねの"
+                  "はばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんゔゕゖゝゞ")
+
+
+def h3_row_nb(node):
+    nname = node.getchildren()[0].text
+    if nname in ["部首", "画数", "音読み", "訓読み"]:
+        nb_row = node.attrib.get("rowspan")
+        if nb_row is None:
+            nb_row = 1
+        else:
+            nb_row = int(nb_row)
+    else:
+        nb_row = 0
+    return nb_row
+
+
+class CExample:
+    def __init__(self, word, reading, sentence):
+        self.word = word
+        self.reading = reading
+        self.sentence = sentence
+
+class CYomi:
+    def __init__(self, reading, isHyoGai):
+        self.isOn = reading[0] in katakana_chart
+        self.isHyoGai = isHyoGai
+        self.reading = reading
+        self.formated = re.sub("（", '<span class="okuri">', reading)
+        self.formated = re.sub("）", "</span>", self.formated)
+        self._example_list = []
+        self._example_list.append(CExample("統帥", "トウスイ", "統帥でござる"))
+
+    # Return the type of Yomi as a list of Boolean: On, OnGai, Kun, KunGai
+    def GetType(self):
+        #        print("XXXX:" + str(self.isOn) + str(self.isHyoGai) + str(self.isOn and self.isHyoGai))
+        return [self.isOn and not self.isHyoGai,
+                self.isOn and self.isHyoGai,
+                not self.isOn and not self.isHyoGai,
+                not self.isOn and self.isHyoGai]
+
+    def tmp_enrich_with_reading_table(self, read_tbl):
+        tree = html.fromstring(read_tbl)
+        read_list = tree.xpath('//*[@class="C_read"]')
+        #read_list = tree.cssselect('//[@class="C_read"]')
+        for read_item in read_list:
+            content = lxml.html.tostring(read_item, encoding='unicode')
+            targ = '<td class="C_read">' + self.formated + '</td>'
+            if content == targ:
+                print(self.formated + self.reading)
+
+class CKanji:
+    listFields = ['Kanji', 'Onyomi', 'Kunyomi', 'Nanori', 'English', 'Examples', 'JLPT Level', 'Jouyou Grade',
+                  'Frequency', 'Components',
+                  'Number of Strokes', 'Kanji Radical', 'Radical Number', 'Radical Strokes', 'Radical Reading',
+                  'Traditional Form',
+                  'Classification', 'Keyword', 'Traditional Radical', 'Reading Table',
+                  'kjBushu', 'kjBushuMei', 'kjKanjiKentei', 'kjBunrui', 'kjIjiDoukun']
+
+    def __init__(self, kanji):
+        self._Kanji = kanji
+        self._propList = []
+        self._readingList = []
+
+    def __iter__(self):
+        return iter(self._propList)
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        else:
+            return False
+
+    def __getitem__(self, key):
+        idx = CKanji.listFields.index(key)
+        return self._propList[idx]
+
+    def __setitem__(self, key, value):
+        idx = CKanji.listFields.index(key)
+        self._propList[idx] = value
+
+    def SetFromRow(self, row):
+        for idx in range(len(row)):
+            self._propList = row
+
+    def _get_kanji_file_name(self):
+        root_dir = r'E:\CloudStorage\Google Drive\Kanji\資料\\'
+        return root_dir + r'Kanji\Kanji_' + self._Kanji + '.html'
+
+    def ProcessJitenon(self):
+        page = pickle.load(open(self._get_kanji_file_name(), "rb"));
+        tree = html.fromstring(page.content)
+
+        block = tree.xpath('//*[@id="kanjiright"]/table/tr/th')
+        startIdx = h3_row_nb(block[0]) + h3_row_nb(block[1])
+        endIdx = startIdx + h3_row_nb(block[2]) + h3_row_nb(block[3])
+
+        # issue with kanji.jineton for 平
+        if self._Kanji == "平":
+            endIdx += 1
+
+        block = tree.xpath('//*[@id="kanjiright"]/table/tr/td')
+        for idx in range(startIdx, endIdx):
+            read = block[idx].getchildren()[0].text
+            self._readingList.append(CYomi(read, "△" in block[idx].text))
+
+        self['Jouyou Grade'] = jsonpickle.encode(self._readingList)
+        #for yomi in self._readingList:
+        #    yomi.tmp_enrich_with_reading_table(self["Reading Table"])
+
+    def GetNbOfReading(self):
+        nbOfReading = [0, 0, 0, 0]
+        for yomi in self._readingList:
+            nbOfReading = [sum(x) for x in zip(nbOfReading, yomi.GetType())]
+        return (nbOfReading)
+
+    def hasGaiYomi(self):
+        hasGai = [False, False]
+        for yomi in self._readingList:
+            hasGai = [x or y for x, y in zip(hasGai, [yomi.GetType()[i] for i in (1, 3)])]
+        return (hasGai)
+
+    def get_json(self):
+        return jsonpickle.encode(self)
