4465cdb11d01ceb889c19bb0f452224c33f97f9e
Fred <test@gitmail.com>
2018-09-26 22:08:59 +0900

    kukan-3 諺 detail screen should display the normal text if furigana is missing

--- kukan/templatetags/ja_tags.py
+++ kukan/templatetags/ja_tags.py
@@ -6,7 +6,12 @@ register = template.Library()
 
 
 @register.filter(is_safe=True)
-def furigana_simple_to_html(furigana_text):
+def furigana_html(plain_text, furigana_text):
     """Convert furigana from simple format (square brackets) to html format (ruby tags)"""
-    JpText.from_furigana_text(furigana_text)
-    return mark_safe(JpText.from_furigana_text(furigana_text).get_furigana_html())
+
+    if furigana_text:
+        JpText.from_furigana_text(furigana_text)
+        res = mark_safe(JpText.from_furigana_text(furigana_text).get_furigana_html())
+    else:
+        res = plain_text
+    return res
